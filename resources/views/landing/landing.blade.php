6e7a40b277db2b1c9faeb2c78f1fba7416e74e8a
raydanielg <airezra2+1@gmail.com>
2026-04-17 14:15:22 +0300

    commit

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -14,8 +14,8 @@
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
     
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <!-- Lucide Icons -->
+    <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.min.js"></script>
     
     <!-- Alpine.js -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>

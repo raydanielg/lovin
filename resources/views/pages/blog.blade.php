619a3eba19c86fc920a5346a8f0ff88430b1629b
raydanielg <airezra2+1@gmail.com>
2026-04-17 14:27:31 +0300

    commit

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -7,8 +7,8 @@
     <link rel="icon" type="image/png" href="{{ asset('transparentslogo/tranp-green.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
-    <!-- Lucide Icons -->
-    <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.min.js"></script>
+    <!-- RemixIcon -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>

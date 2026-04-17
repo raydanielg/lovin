cff89b2e2d8f3385ab1e5bcf2520170ae3e9eb71
raydanielg <airezra2+1@gmail.com>
2026-04-17 14:19:07 +0300

    commit

--- resources/views/pages/industries.blade.php
+++ resources/views/pages/industries.blade.php
@@ -7,7 +7,8 @@
     <link rel="icon" type="image/png" href="{{ asset('transparentslogo/tranp-green.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <!-- Lucide Icons -->
+    <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
